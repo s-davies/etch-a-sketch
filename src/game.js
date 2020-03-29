f0ab22812e17cf7a7541611a6cad755ea89fc443
s-davies <swdavies.contact@gmail.com>
2020-03-29 15:05:55 -0700

    test shake

--- src/game.js
+++ src/game.js
@@ -154,14 +154,8 @@ export default class EtchASketch {
 
     myShakeEvent.start();
 
-    window.addEventListener('shake', shakeEventDidOccur, false);
-
-    //function to call when shake occurs
-    function shakeEventDidOccur() {
-
-      //put your own code here etc.
-      alert('shake!');
-    }
+    window.addEventListener('shake', this.clearSketch, false);
+    
   }
   //////////////////////////////////////////////////////////////////////////////
 
